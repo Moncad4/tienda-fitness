64ec6fa0b6dfeb5330fa71272732dac3c0192997
juan david moncada <juadavidmoncada7@gmail.com>
2024-07-15 18:25:25 -0500

    componente ayudas

--- View/components/ayudas.php
+++ View/components/ayudas.php
@@ -8,42 +8,122 @@
   <link rel="stylesheet" href="../css/ayudas.css">
 </head>
 <body>
-  <header class="container-header">
-    <nav class="navegador">
-      <div class="contenedor-logo">
-        <a href="./principal.php"><h2>Logo</h2></a>
+  <div class="background-ayudas">
+    <header class="container-header">
+      <nav class="navegador">
+        <div class="contenedor-logo">
+          <a href="../principal.php"><h2>Logo</h2></a>
+        </div>
+        <div class="div-list-nav">
+          <ul class="list-nav">
+            <li><a href="../principal.php">Inicio</a></li>
+            <li><a href="./tienda.php">Tienda</a></li>
+            <li><a href="#">Nosotros</a></li>
+            <li><a href="./ayudas.php">Ayudas</a></li>
+            <li><a href="#">Contacto</a></li>
+            <li class="inicio-sesion">
+              <a href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                  <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                  <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                </svg>Iniciar Sesion</a>
+            </li>
+            <li>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                  <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                  <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                  <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+                  <path d="M17 17h-11v-14h-2" />
+                  <path d="M6 5l14 1l-1 7h-13" />
+                </svg>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+
+    <section class="section-comp-ayudas">
+      <h2>Ayudas</h2>
+      <div class="container-comp-ayudas">
+        <a href="#">
+          <div class="subContainer-comp-ayudas grasa-ayudas">
+            <img src="../img/ayudas/perdida-grasa.jpg" alt="">
+          </div>
+          <div class="subContainer2-comp-ayudas">
+            <h3>¿Como quemar grasa?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt architecto voluptatum dolor 
+            reprehenderit cupiditate ullam sit libero itaque dolore molestias beatae aperiam ratione quasi.</p>
+          </div>
+        </a>
       </div>
-      <div class="div-list-nav">
-        <ul class="list-nav">
+
+      <div class="container-comp-ayudas agua-ayudas">
+        <a href="#">
+          <div class="subContainer-comp-ayudas">
+            <img src="../img/ayudas/tomar-agua.jpg" alt="">
+          </div>
+          <div class="subContainer2-comp-ayudas">
+            <h3>¿Por que tomar mas agua?</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore veniam dicta dignissimos
+            facere, corrupti eius, cum qui eaque eum earum ratione praesentium omnis quod error. Sapiente doloribus!</p>
+          </div>
+        </a>
+      </div>
+
+      <div class="container-comp-ayudas dieta-ayudas">
+        <a href="">
+          <div class="subContainer-comp-ayudas">
+            <img src="../img/ayudas/dieta.jpg" alt="">
+          </div>
+          <div class="subContainer2-comp-ayudas">
+            <h3>Dietas para ganar masa muscular</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam soluta distinctio repellendus facilis minima! 
+            Commodi omnis saepe debitis aspernatur voluptate eaque repellat doloribus est amet, numquam quibusdam!</p>
+          </div>
+        </a>
+      </div>
+    </section>
+  </div>
+
+  <footer class="footer">
+    <div class="container-footer">
+      <div class="logo-footer">
+        <h1>Logo</h1>
+      </div>
+      <div class="menu-footer">
+        <h3>Menu</h3>
+        <ul>
           <li><a href="#">Inicio</a></li>
           <li><a href="#">Tienda</a></li>
           <li><a href="#">Nosotros</a></li>
           <li><a href="#">Ayudas</a></li>
           <li><a href="#">Contacto</a></li>
-          <li class="inicio-sesion">
-            <a href="#">
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-              </svg>
-            <a href="#">Iniciar Sesion</a>
-          </li>
-          <li>
-            <button>
-              <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="25" height="25" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                <path d="M6 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                <path d="M17 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
-                <path d="M17 17h-11v-14h-2" />
-                <path d="M6 5l14 1l-1 7h-13" />
-              </svg>
-            </button>
-          </li>
         </ul>
       </div>
-    </nav>
-  </header>
+      <div class="politicas-footer">
+        <h3>Politicas</h3>
+        <ul>
+          <li><a href="#">FAQ</a></li>
+          <li><a href="#">Politica de la tienda</a></li>
+        </ul>
+      </div>
+      <div class="contacto-footer">
+        <h3>Contacto</h3>
+        <ul>
+          <li>(+57)3006321011</li>
+          <li>Juandavidmoncada7@gmail.com</li>
+          <li>2500 Lotes, Pereira, Colombia</li>
+        </ul>
+      </div>
+    </div>
+    <div class="derechos-footer">
+      <h4>&copy Moncad4 y Jillfeint</h4>
+      <p>Todos los derechos reservados</p>
+    </div>
+  </footer> 
 </body>
 </html>
