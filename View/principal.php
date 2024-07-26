73329b59f07859535872ae9c0979c3bef922d6eb
juan david moncada <juadavidmoncada7@gmail.com>
2024-07-25 20:47:03 -0500

    footer

--- View/principal.php
+++ View/principal.php
@@ -145,42 +145,9 @@
     </div>
   </section>
 
-  <footer class="footer">
-    <div class="container-footer">
-      <div class="logo-footer">
-        <h1>Logo</h1>
-      </div>
-      <div class="menu-footer">
-        <h3>Menu</h3>
-        <ul>
-          <li><a href="#">Inicio</a></li>
-          <li><a href="#">Tienda</a></li>
-          <li><a href="#">Nosotros</a></li>
-          <li><a href="#">Ayudas</a></li>
-          <li><a href="#">Contacto</a></li>
-        </ul>
-      </div>
-      <div class="politicas-footer">
-        <h3>Politicas</h3>
-        <ul>
-          <li><a href="#">FAQ</a></li>
-          <li><a href="#">Politica de la tienda</a></li>
-        </ul>
-      </div>
-      <div class="contacto-footer">
-        <h3>Contacto</h3>
-        <ul>
-          <li>(+57)3006321011</li>
-          <li>Juandavidmoncada7@gmail.com</li>
-          <li>2500 Lotes, Pereira, Colombia</li>
-        </ul>
-      </div>
-    </div>
-    <div class="derechos-footer">
-      <h4>&copy Moncad4 y Jillfeint</h4>
-      <p>Todos los derechos reservados</p>
-    </div>
-  </footer>
+  <?php
+  require './footer/footer.php'
+  ?>
   <script src="./js/inicio.js"></script>
 </body>
 </html>
