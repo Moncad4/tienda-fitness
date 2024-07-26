20734a95ba84eeb386321cc1c4802a5d70acd2c8
Jill Angel <tpennywise3@hotmail.com>
2024-07-25 20:01:39 -0500

    tienda lista

--- View/components/tienda.php
+++ View/components/tienda.php
@@ -82,9 +82,6 @@
             <div class="articleshop-maincards artmaincard1">
               <img class="products-shop" src="../img/tienda/product1.jpeg" alt="">
               <div class="product-information">
-                <p class="addcart-text">
-                    Info
-                  </p>  
                 <button class="cart-btn">
                   <span class="icon-container"> 
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
@@ -93,14 +90,14 @@
                     Add to Cart
                   </p>
                 </button>   
-              </div> g
+                <p class="addcart-text addcart-textindi">
+                    Detalles...
+                </p>  
+              </div>
             </div>
             <div class="articleshop-maincards artmaincard2">
               <img class="products-shop" src="../img/tienda/product2.jpg" alt="">
-              <div class="product-information">
-                <p class="addcart-text">
-                    Info
-                  </p>  
+              <div class="product-information"> 
                 <button class="cart-btn">
                   <span class="icon-container"> 
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
@@ -108,15 +105,15 @@
                   <p class="addcart-text">
                     Add to Cart
                   </p>
-                </button>   
+                </button>  
+                <p class="addcart-text addcart-textindi">
+                    Detalles...
+                </p>   
               </div> 
             </div>
             <div class="articleshop-maincards artmaincard3">
               <img class="products-shop" src="../img/tienda/product3.jpg" alt="">
               <div class="product-information">
-                <p class="addcart-text">
-                    Info
-                  </p>  
                 <button class="cart-btn">
                   <span class="icon-container"> 
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512" fill="rgb(17, 17, 17)" class="cart"><path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path></svg>
@@ -124,7 +121,10 @@
                   <p class="addcart-text">
                     Add to Cart
                   </p>
-                </button>   
+                </button> 
+                <p class="addcart-text addcart-textindi">
+                    Detalles...
+                </p>    
               </div> 
             </div>
             <div class="articleshop-maincards artmaincard4">2</div>
