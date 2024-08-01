dc6109c251974bc63928c0ae2c93bff645a1d25e
juan david moncada <juadavidmoncada7@gmail.com>
2024-07-31 19:25:08 -0500

    base de datos

---
+++ database/DB.php
@@ -0,0 +1,21 @@
+<?php
+class Database{
+  private $host = 'localhost';
+  private $username = 'root';
+  private $password = '';
+  private $datebase = 'loginFitness';
+  private $conn;
+
+  public function __construct(){
+    $this->conn = new mysqli($this->host, $this->username, $this->password, $this->datebase);
+
+    if ($this->conn->connect_error){
+      die("Error de conexión a la base de datos: " . $this->conn->connect_error);
+    }
+  }
+    
+  public function getConnection(){
+    return $this->conn;
+  }
+  }
+?>
