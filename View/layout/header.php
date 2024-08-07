ea909429f5cb032a5402c00b8930393ad52c1e7c
juan david moncada <juadavidmoncada7@gmail.com>
2024-08-07 16:41:44 -0500

    svg de inicio de sesion

--- View/layout/header.php
+++ View/layout/header.php
@@ -28,7 +28,8 @@ $userPerfil = $perfil->showProfileHeader();
                       <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
                       <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
                       <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-                    </svg>Cuenta&#11167;
+                    </svg>
+                    <span>Cuenta&#11167;</span>
                   </a>
                   <ul>
                     <li><a href="<?php echo BASE_URL; ?>View/components/perfil.php">Perfil</a></li>
@@ -38,12 +39,12 @@ $userPerfil = $perfil->showProfileHeader();
                 </ul>
                 <?php } else { ?>
                   <a href="<?php echo BASE_URL; ?>View/components/iniciarSesion.php">
-                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="40" height="40" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
-                    <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
-                    <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
-                  </svg>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user-circle" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#00b341" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                      <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                      <path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" />
+                      <path d="M12 10m-3 0a3 3 0 1 0 6 0a3 3 0 1 0 -6 0" />
+                      <path d="M6.168 18.849a4 4 0 0 1 3.832 -2.849h4a4 4 0 0 1 3.834 2.855" />
+                    </svg>
                   <span>Iniciar Sesion</span>
                   </a>
                 <?php } ?>
