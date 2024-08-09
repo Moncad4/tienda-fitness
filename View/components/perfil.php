37bd977f0ce2de1f7885cca0172215caf0cb5fa3
juan david moncada <juadavidmoncada7@gmail.com>
2024-08-09 16:13:26 -0500

    cambios nuevos

--- View/components/perfil.php
+++ View/components/perfil.php
@@ -36,7 +36,16 @@ $userPerfil = $perfil->showProfile();
           </svg>
           <span>Nombre: <?php echo $userPerfil['NickName']?></span>
         </h2>
-        <h2>Apellido: <?php echo $userPerfil['LastName']?></h2>
+
+        <h2>
+          <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
+            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+            <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+            <path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+          </svg>
+          <span>Apellido: <?php echo $userPerfil['LastName']?></span>
+        </h2>
+
         <h2>
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-mail" width="35" height="35" viewBox="0 0 24 24" stroke-width="1.5" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
           <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
