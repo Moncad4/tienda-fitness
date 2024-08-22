a235ba6c0f11c4733e5a9fcb8f1ca2cda2f0675e
Jill Angel <tpennywise3@hotmail.com>
2024-08-22 11:57:54 -0500

    base de datos

--- tiendafitness.sql
+++ tiendafitness.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 22-08-2024 a las 05:52:15
+-- Servidor: localhost:3310
+-- Tiempo de generación: 22-08-2024 a las 18:55:54
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -42,11 +42,12 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`product_id`, `tittle`, `description`, `price`, `image`, `discount`, `priceDiscount`) VALUES
-(2, 'super power', 'sano fuerte y lindo', 150000, '', 3000, 147000),
-(3, 'protein loco', 'te vuelve loco', 200000, '', 10000, 190000),
-(4, 'calcio', 'fuerza en los huesos', 250000, '', 20000, 230000),
-(6, 'no me grite', 'que bebesote', 300000, 0x696d6167655f363662613866346538363338322e6a7067, 120000, 180000),
-(7, 'evolution fit', 'te evoluciona a tu limite', 200000, 0x696d6167655f363662616463646233653934622e6a7067, 50000, 150000);
+(8, 'Mother Bucker', 'Es un pre-entrenamiento potente de Bucked Up, diseñado para atletas experimentados que buscan aumentar su energía, enfoque y rendimiento. Contiene 400 mg de cafeína y otros ingredientes avanzados para maximizar el bombeo muscular y la resistencia​.', 350000, 0x696d6167655f363663366363373335383037392e6a706567, 50000, 30000),
+(9, 'Creatine Turbo', 'Es un suplemento que aumenta la fuerza y el rendimiento físico. Contiene creatina monohidrato, que ayuda a mejorar la producción de energía en los músculos. Se usa comúnmente para ganar masa muscular y acelerar la recuperación.', 450000, 0x696d6167655f363663366364313834653536622e6a7067, 25000, 425000),
+(10, 'Psychotic', 'Es un suplemento pre-entrenamiento que ofrece energía intensa, enfoque y resistencia. Suele contener ingredientes como cafeína, beta-alanina y otros estimulantes para mejorar el rendimiento durante los entrenamientos.', 250000, 0x696d6167655f363663366364373632366163332e6a7067, 10000, 240000),
+(11, 'ISO Whey Zero ', 'Es un suplemento de proteína en polvo, hecho principalmente de aislado de proteína de suero. Es bajo en grasas y carbohidratos, ideal para la recuperación muscular y el desarrollo de masa magra. Se disuelve fácilmente y es adecuado para dietas bajas en lactosa.', 220000, 0x696d6167655f363663366365336536656564392e6a706567, 10000, 210000),
+(14, 'ISO 100 Hydrolyzed', 'Es un suplemento de proteína de suero hidrolizada. Ofrece una rápida absorción, baja en grasas y carbohidratos, ideal para la recuperación y el desarrollo muscular. Es especialmente útil para quienes buscan una proteína de alta calidad y fácil digestión.', 380000, 0x696d6167655f363663366366656134323233352e6a706567, 70000, 310000),
+(15, '3 Whey Protein', 'Es un suplemento de proteína en polvo que combina tres tipos de proteína de suero: concentrado, aislado e hidrolizado. Proporciona una liberación gradual de aminoácidos, ideal para soporte muscular continuo y recuperación.', 700000, 0x696d6167655f363663366430363162363430312e6a7067, 240000, 460000);
 
 -- --------------------------------------------------------
 
@@ -93,7 +94,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT de la tabla `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `product_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT de la tabla `users`
